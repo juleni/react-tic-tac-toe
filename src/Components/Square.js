acff0aad07ce5f5813daa91eab146e1c86f3a769
juleni <69912179+juleni@users.noreply.github.com>
2021-06-02 11:24:28 +0200

    Changed Square component to FUNCTION COMPONENT

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,30 +1,32 @@
 
-import React from 'react';
 import "../Square.css";
 
-class Square extends React.Component {
+function Square(props) {
+  // Square component is FUNCTION COMPONENT that only contain a 'render'
+  // method and don't have their own state. Instead of defining a class
+  // which extends React.Component, we can write function that takes 'props'
+  // as an input and returns what should be rendered.
+
   // Constructor is not here becouse Square component no longer 
   // keeps track of the game's state.
-  render () {
-    return(
-      // Set state value to X when clicked on the square.
-      // Calling setStare in a component, React automatically updates
-      // the child components inside of it too.
-      <button 
-        className="square" 
-        // Props 'value' and 'onClick' are passing down from Board component.
-        // When a Square is clicked, the onClick function provided by Board 
-        // is called. Since the Board passed onClick={() => this.handleClick(i)}
-        // to Square, the Square calls this.handleClick(i) when clicked.
+  return(
+    // Set state value to X when clicked on the square.
+    // Calling setStare in a component, React automatically updates
+    // the child components inside of it too.
+    <button 
+      className="square" 
+      // Props 'value' and 'onClick' are passing down from Board component.
+      // When a Square is clicked, the onClick function provided by Board 
+      // is called. Since the Board passed onClick={() => this.handleClick(i)}
+      // to Square, the Square calls this.handleClick(i) when clicked.
 
-        // In React, it's conventional to use on[Event] names for methods which
-        // represent events.        
-        onClick={() => this.props.onClick()}
-      >
-        { this.props.value }
-      </button>
-    );
-  }
+      // In React, it's conventional to use on[Event] names for methods which
+      // represent events.        
+      onClick={props.onClick}
+    >
+      { props.value }
+    </button>
+  );
 }
 
 export default Square;
