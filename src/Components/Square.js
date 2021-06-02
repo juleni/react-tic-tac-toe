c97471bbf19907df1fe1c7fc23562d49ad094685
juleni <69912179+juleni@users.noreply.github.com>
2021-06-02 10:08:35 +0200

    Added prop value that will be populated to the Square component

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -3,10 +3,27 @@ import React from 'react';
 import "../Square.css";
 
 class Square extends React.Component {
+  constructor(props) {
+    // Allways call "super" when defining the constructor of a subclass.
+    // All React components classes that have constructor should start 
+    // with a super(props) call.
+    super(props);
+    // initialize the state
+    this.state = {
+      value: null,
+    };
+  }
+
   render () {
     return(
-      <button className="square">
-        { /* TODO */}
+      // Set state value to X when clicked on the square.
+      // Calling setStare in a component, React automatically updates
+      // the child components inside of it too.
+      <button 
+        className="square" 
+        onClick={() => this.setState({value: 'X'})}
+      >
+        { this.state.value }
       </button>
     );
   }
